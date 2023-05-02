2fb2864ebb0de86039541a48ba97b7eba1e219ec
Known Black Hat <65008967+KnownBlackHat@users.noreply.github.com>
2023-05-02 22:01:29 +0000

    fix: button error embed

--- Cogs/delete_button_listner.py
+++ Cogs/delete_button_listner.py
@@ -14,7 +14,7 @@ class DeleteButtonListner(commands.Cog):
         if interaction.author.id == interaction.message.interaction.author.id:
             await interaction.delete_original_message()
         else:
-            await interaction.send(embed=Embeds.emb(":cry: This button is not for you"))
+            await interaction.send(embed=Embeds.emb(Embeds.red, ":cry: This button is not for you"))
 
 
 def setup(client: commands.Bot):
