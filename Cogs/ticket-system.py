c104be2a2ab36f8480eedebf0500e59bc3f0a3fe
Known_Black_Hat <knownblackhat@gmail.com>
2023-05-30 18:24:57 -0400

    fix: duplicate tickets

--- Cogs/ticket-system.py
+++ Cogs/ticket-system.py
@@ -5,20 +5,26 @@ import disnake
 from aiosqlite import Connection
 from disnake.ext import commands
 
-from utils import Embeds, delete_button
+from utils import Embeds
 
+logger = logging.getLogger(__name__)
 new_ticket_button: disnake.ui.Button = disnake.ui.Button(
     emoji="🎫",
     style=disnake.ButtonStyle.blurple,
     custom_id="new_ticket",
 )
-logger = logging.getLogger(__name__)
+delete_channel_button: disnake.ui.Button = disnake.ui.Button(
+    emoji="🗑️",
+    style=disnake.ButtonStyle.red,
+    custom_id="delete_channel",
+    label="Close Ticket",
+)
 
 
 class Ticket(disnake.ui.Modal):
     def __init__(
         self,
-        color,
+        color=None,
         db: Optional[Connection] = None,
         category: Optional[disnake.CategoryChannel] = None,
         user_or_role: Optional[Union[disnake.Role, disnake.Member]] = None,
@@ -74,19 +80,19 @@ class Ticket(disnake.ui.Modal):
         if await (
             await self.db.execute(
                 """
-                                       select guild_id from tickets where
-                                       guild_id = ?
-                                       """,
+                    select guild_id from tickets where
+                    guild_id = ?
+                    """,
                 (interaction.guild.id,),
             )
         ).fetchone():
             await self.db.execute(
                 """
-                                  update tickets set
-                                  guild_id = ?, user_or_role_id = ?,
-                                  category_id = ?, image = ?, title = ?,
-                                  description = ? where guild_id = ?
-                                  """,
+                    update tickets set
+                    guild_id = ?, user_or_role_id = ?,
+                    category_id = ?, image = ?, title = ?,
+                    description = ? where guild_id = ?
+                    """,
                 (
                     interaction.guild.id,
                     self.user_or_role.id,
@@ -100,11 +106,11 @@ class Ticket(disnake.ui.Modal):
         else:
             await self.db.execute(
                 """
-                                  insert into tickets
-                                  (guild_id, user_or_role_id, category_id,
-                                   image, title, description)
-                                  values (?, ?, ?, ?, ?, ?)
-                                  """,
+                    insert into tickets
+                    (guild_id, user_or_role_id, category_id,
+                     image, title, description)
+                    values (?, ?, ?, ?, ?, ?)
+                    """,
                 (
                     interaction.guild.id,
                     self.user_or_role.id,
@@ -115,8 +121,10 @@ class Ticket(disnake.ui.Modal):
                 ),
             )
         await self.db.commit()
+        await interaction.send("Ticket Setup Complete", ephemeral=True)
 
     async def on_error(self, error: Exception, inter: disnake.ModalInteraction):
+        logger.exception(error)
         await inter.response.send_message(
             embed=Embeds.emb(Embeds.red, "Oops! Something went wrong :cry:"),
             ephemeral=True,
@@ -147,64 +155,86 @@ class TicketSystem(commands.Cog):
         interaction: disnake.GuildCommandInteraction,
         category: disnake.CategoryChannel,
         user_or_role: Union[disnake.Role, disnake.Member],
-        color: Optional[disnake.Color] = None,
     ):
         """Setup the ticket system"""
         await self.bot.db.execute(
             """
-                                  create table if not exists tickets
-                                  ('guild_id' int, 'user_or_role_id' int,
-                                   'category_id' int, 'image' string,
-                                   'title' string, 'description' string)
-                                  """
+                create table if not exists tickets
+                ('guild_id' int, 'user_or_role_id' int,
+                 'category_id' int, 'image' string,
+                 'title' string, 'description' string)
+                """
         )
         await interaction.response.send_modal(
             modal=Ticket(
                 db=self.bot.db,
                 category=category,
                 user_or_role=user_or_role,
-                color=color,
             )
         )
 
     @commands.Cog.listener()
     async def on_button_click(self, interaction: disnake.MessageInteraction):
-        if interaction.component.custom_id != "new_ticket" or not interaction.guild:
+        if not interaction.guild:
             return
-        result = await (
-            await self.bot.db.execute(
-                """
-                                           select guild_id, user_or_role_id,
-                                             category_id from tickets where
-                                           guild_id = ?
-                                           """,
-                (interaction.guild.id,),
-            )
-        ).fetchone()  # type: ignore
-        if result:
-            (_, user_or_role_id, category_id) = result
-            category = interaction.guild.get_channel(category_id)
-            if not category:
-                return
-            channel = await interaction.guild.create_text_channel(
-                f"ticket-{interaction.user.name}",
-                category=category,  # type: ignore
-                overwrites={  # type: ignore
-                    interaction.guild.default_role: disnake.PermissionOverwrite(
-                        read_messages=False
+        elif interaction.component.custom_id == "delete_channel":
+            await interaction.channel.delete()  # type: ignore
+        elif interaction.component.custom_id == "new_ticket":
+            result = await (
+                await self.bot.db.execute(
+                    """
+                    select guild_id, user_or_role_id,
+                    category_id, image, title, description
+                    from tickets where guild_id = ?
+                    """,
+                    (interaction.guild.id,),
+                )
+            ).fetchone()  # type: ignore
+            if result:
+                (_, user_or_role_id, category_id, image, title, description) = result
+                category = interaction.guild.get_channel(category_id)
+                if not category:
+                    return
+                channels = {c.name for c in interaction.guild.get_channel(category_id).text_channels}  # type: ignore
+                if f"{interaction.user.name}-{interaction.user.id}".lower() in channels:
+                    await interaction.send(
+                        embed=Embeds.emb(
+                            Embeds.red,
+                            "You already have a ticket open!",
+                            "Please close the previous ticket to open a new one!",
+                        ),
+                        ephemeral=True,
+                    )
+                    return
+                channel = await interaction.guild.create_text_channel(
+                    f"{interaction.user.name}-{interaction.user.id}",
+                    category=category,  # type: ignore
+                    overwrites={  # type: ignore
+                        interaction.guild.default_role: disnake.PermissionOverwrite(
+                            read_messages=False
+                        ),
+                        interaction.user: disnake.PermissionOverwrite(
+                            read_messages=True
+                        ),
+                        interaction.guild.get_role(
+                            user_or_role_id
+                        ): disnake.PermissionOverwrite(read_messages=True),
+                        interaction.guild.me: disnake.PermissionOverwrite(
+                            read_messages=True
+                        ),
+                    },
+                )
+                await channel.send(
+                    embed=Embeds.emb(Embeds.green, title, description)
+                    .set_image(image)
+                    .set_footer(
+                        text=interaction.guild.name, icon_url=interaction.guild.icon
                     ),
-                    interaction.user: disnake.PermissionOverwrite(read_messages=True),
-                    interaction.guild.get_role(
-                        user_or_role_id
-                    ): disnake.PermissionOverwrite(read_messages=True),
-                    interaction.guild.me: disnake.PermissionOverwrite(
-                        read_messages=True
-                    ),
-                },
-            )
-            await interaction.send(
-                f"Ticket has been created {channel.mention}", ephemeral=True
-            )
+                    components=[delete_channel_button],
+                )
+                await interaction.send(
+                    f"Ticket has been created {channel.mention}", ephemeral=True
+                )
 
 
 def setup(client: commands.Bot):
